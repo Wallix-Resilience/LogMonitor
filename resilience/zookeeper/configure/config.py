fb2ad17f2ce1890ba015a73402386d21f218dc37
lahoucine <lahoucine@benlahmr.com>
2012-07-19 14:00:20 +0200

    config functions completed

--- resilience/zookeeper/configure/config.py
+++ resilience/zookeeper/configure/config.py
@@ -2,6 +2,7 @@
 
 from txzookeeper.client import ZookeeperClient
 from txzookeeper.retry import RetryClient
+import zookeeper
 import sys
 
 from twisted.python import log
@@ -15,65 +16,52 @@ class Config():
     def __init__(self, zkAddr):
         self.zk = zkAddr
         
-    
-    def add_mongod(self, mongod):
-        self.zk.create("/monggodb/mongod/")
+    def _err(self,error):
+        log.msg('node seems to already exists : %s' % error)
         
-    
+    def add_mongod(self, mongod):
+        d = self.zk.create("/monggodb/mongod/%s" % str(mongod))
+        d.addErrback(self._err)
+                        
     def add_solr(self, solr):
-        self.zk.create("/solr/")
+        d =  self.zk.create("/solr/%s" % str(solr))
+        d.addErrback(self._err)
     
-    def get_mongod(self, callback=None):
-
-        if callback == None:
-            #get children
-            pass
-        else:
-            def _call(self):
-                pass
-            #get children and watch
-            pass
+    def get_mongod_all(self, callback = None):
+        self._get_conf("/mongo", callback)
                    
-    def get_solr(self):
-        pass
+    def get_solr_all(self, callback = None):
+        self._get_conf("/solr", callback)
     
-    def _get_conf(self, path, callback = None):
+    def _get_conf_all(self, path, callback = None):
         
         def _get_value(m):
-            value = m[0]
+            value = m
             if callback:
-                callback(value)
-            print  value
-            
+                callback(value)   
         def _call(m):
-            print "func call"
-            dat, m = self.zk.get_and_watch(path)
+            dat, m = self.zk.get_children_and_watch(path)
             dat.addCallback(_get_value)
-            print "get res", dat.returnValue()
             m.addCallback(_call)
         
         if not callback:
             data = self.zk.get(path)
             data.addCallback(_get_value)
-            
         else:
-            data, diff = self.zk.get_and_watch(path)
+            data, diff = self.zk.get_children_and_watch(path)
             data.addCallback(_get_value)
             diff.addCallback(_call)
             
-        
-        
-    
     
 if __name__ == "__main__":
     def cb_connected(self, zc):
         cfg = Config(zc)
         def _call(m):
             print "call",m
-        res = cfg._get_conf("/test",_call)
-        print "res", res
-        
-        
+        cfg._get_conf("/test",_call)
+        cfg.get_solr(_call)
+        cfg.get_mongod(_call)
+        cfg.add_solr('localhost6:20120')
     zc = RetryClient(ZookeeperClient("127.0.0.1:2181"))
     d = zc.connect()
     d.addCallback(cb_connected, zc)
